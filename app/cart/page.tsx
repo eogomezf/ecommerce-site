f30f2c25dd022fbf213464bfdb36f79170c4f360
Elvis Gomez <eogomezf@gmail.com>
2025-05-19 17:30:42 -0600

    Creating a shopping cart list

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { products } from "../product-data";
+import Link from "next/link";
 
-function Cart() {
-  return <div>Cart</div>;
+function CartPage() {
+  const [cartIds] = useState(["123", "345"]);
+
+  const cartProducts = cartIds.map(
+    (id) => products.find((product) => product.id === id)!
+  );
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+      {cartProducts.map((product) => (
+        <Link
+          key={product.id}
+          href={`/products/${product.id}`}
+          className="flex items-center justify-between p-4 border-b"
+        >
+          {/* <img src={product.imageUrl} alt={product.name} className="w-16 h-16 object-cover" /> */}
+          <div className="flex-1 ml-4">
+            <h2 className="text-lg font-bold">{product.name}</h2>
+            <p>{product.description}</p>
+            <p className="text-lg font-semibold">${product.price}</p>
+          </div>
+          <button className="bg-red-500 text-white px-4 py-2 rounded">
+            Remove
+          </button>
+        </Link>
+      ))}
+    </>
+  );
 }
 
-export default Cart;
+export default CartPage;
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,28 @@
 import { products } from "@/app/product-data";
+import Image from "next/image";
 
 function productDetail({ params }: { params: { id: string } }) {
   const product = products.find((product) => product.id === params.id);
   if (!product) {
     return <div>Product not found</div>;
   }
-  return <h1>{product.name}</h1>;
+  return (
+    <>
+      <h1 className="text-2xl font-bold">{product.name}</h1>
+      <Image
+        src={"/" + product.imageUrl}
+        alt={product.name}
+        width={100}
+        height={100}
+        className="w-full h-48 object-cover rounded"
+      />
+      <p className="text-lg font-semibold">${product.price}</p>
+      <p>{product.description}</p>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded">
+        Add to Cart
+      </button>
+    </>
+  );
 }
 
 export default productDetail;
