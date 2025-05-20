22abc48398fa8794131f5ef60c1ebaf9b38d1c8a
Elvis Gomez <eogomezf@gmail.com>
2025-05-20 14:18:39 -0600

    modifiy delete method

--- app/api/users/[id]/cart/route.ts
+++ app/api/users/[id]/cart/route.ts
@@ -52,15 +52,23 @@ export async function POST(
   request: NextRequest,
   { params }: { params: Params }
 ) {
+  const { db } = await connectToDb();
   const userId = params.id;
   const body: CartBody = await request.json();
   const productId = body.productId;
 
-  carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId];
+  const updatedCart = await db
+    .collection("carts")
+    .findOneAndUpdate(
+      { userId },
+      { $push: { cartIds: productId } },
+      { upsert: true, returnDocument: "after" }
+    );
 
-  const cartProducts = carts[userId].map((id) =>
-    products.find((product) => product.id === id)
-  );
+  const cartProducts = await db
+    .collection("products")
+    .find({ id: { $in: updatedCart.cartIds } })
+    .toArray();
 
   return new Response(JSON.stringify(cartProducts), {
     status: 201,
@@ -72,21 +80,30 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: Params }
 ) {
+  const { db } = await connectToDb();
   const userId = params.id;
   const body: CartBody = await request.json();
   const productId = body.productId;
 
-  if (!carts[userId]) {
-    return new Response("Cart not found", { status: 404 });
+  const updatedCart = await db
+    .collection("carts")
+    .findOneAndUpdate(
+      { userId },
+      { $pull: { cartIds: productId } },
+      { returnDocument: "after" }
+    );
+
+  if (!updatedCart) {
+    return new Response(JSON.stringify([]), {
+      status: 202,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 
-  carts[userId] = carts[userId]
-    ? carts[userId].filter((id) => id !== productId)
-    : [];
-
-  const cartProducts = carts[userId].map((id) =>
-    products.find((product) => product.id === id)
-  );
+  const cartProducts = await db
+    .collection("products")
+    .find({ id: { $in: updatedCart.cartIds } })
+    .toArray();
 
   return new Response(JSON.stringify(cartProducts), {
     status: 202,
