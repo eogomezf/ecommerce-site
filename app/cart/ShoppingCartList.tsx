1c55a24d45ed990ef3ad316fd9c42f22edbf24d9
Elvis Gomez <eogomezf@gmail.com>
2025-05-23 15:42:48 -0600

    remove item from the list

--- app/cart/ShoppingCartList.tsx
+++ app/cart/ShoppingCartList.tsx
@@ -9,7 +9,22 @@ export default function ShoppingCartList({
 }: {
   initialCartProducts: Product[];
 }) {
-  const [cartProducts] = useState(initialCartProducts);
+  const [cartProducts, setCartProducts] = useState(initialCartProducts);
+
+  async function removeFromCart(productId: string) {
+    const response = fetch("http://localhost:3000/api/users/2/cart", {
+      method: "DELETE",
+      body: JSON.stringify({
+        productId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const updatedCartProducts = await (await response).json();
+    setCartProducts(updatedCartProducts);
+  }
 
   return (
     <div className="container mx-auto p-8">
@@ -22,9 +37,30 @@ export default function ShoppingCartList({
             className="bg-white rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300"
           >
             <Link href={`/products/${product.id}`}>
-              <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+              <div>
+                <div className="flex justify-start mb-4 h-25 relative">
+                  <img
+                    src={"/" + product.imageUrl}
+                    alt={product.name}
+                    className="object-cover h-20 w-auto rounded-lg"
+                  />
+                </div>
 
-              <p className="text-gray-600">${product.price}</p>
+                <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+
+                <p className="text-gray-600">${product.price}</p>
+              </div>
+              <div className="flex justify-end">
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    removeFromCart(product.id);
+                  }}
+                >
+                  Remove from Cart
+                </button>
+              </div>
             </Link>
           </li>
         ))}
